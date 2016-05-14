8682d3637f8615eccf9a72d7fe46eac05b87379c
Mihael Konjevic <konjevic@gmail.com>
2016-05-14 21:22:04 +0200

    Build the app

---
+++ app/services/bucket/bucket-model.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// bucket-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bucketSchema = new Schema({
+  createdAt: { type: Date, 'default': Date.now },
+  updatedAt: { type: Date, 'default': Date.now }
+});
+
+const bucketModel = mongoose.model('bucket', bucketSchema);
+
+module.exports = bucketModel;
