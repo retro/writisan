1cc8db2ec573275933c8e53c206e3188c9adbed7
Mihael Konjevic <konjevic@gmail.com>
2016-05-21 15:08:44 +0200

    Set local storage token

--- app/app.js
+++ app/app.js
@@ -14,6 +14,7 @@ const configuration = require('feathers-configuration');
 const hooks = require('feathers-hooks');
 const rest = require('feathers-rest');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const socketio = require('feathers-socketio');
 const middleware = require('./middleware');
 const services = require('./services');
@@ -25,23 +26,24 @@ const Bucket = app.service('/buckets/');
 app.enable("trust proxy", "loopback");
 app.configure(configuration(path.join(__dirname, '..')));
 
+// app.get('/new-bucket', function(req, res) {
+//   Bucket.create().then(bucket => {
+//     res.redirect(302, '/app/' + bucket._id);
+//   }, (err) => {
+//     res.status(500).send('ERROR');
+//   });
+// });
+
+// app.get('/app/:bucketId', function(req, res){
+//   res.send('BUCKET ID:' + req.params.bucketId);
+// });
+
+app.use(compress()).options('*', cors()).use(cors()).use(favicon(path.join(app.get('public'), 'favicon.ico'))).use('/', serveStatic(app.get('public'))).use(cookieParser()).use(bodyParser.json()).use(bodyParser.urlencoded({ extended: true })).get('/auth/success', function (req, res) {
+  res.set('Content-Type', 'text/html');
+  res.send('<script>localStorage.setItem("feathers-jwt", "' + req.cookies["feathers-jwt"] + '"); window.close(); </script>');
+  //res.sendFile(path.resolve(__dirname, '..', 'public', 'close_window.html'));
+}).configure(hooks()).configure(rest()).configure(socketio()).configure(services).configure(middleware);
+
 // Set up our own custom redirect route for successful login
-app.get('/auth/success', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '..', 'public', 'close_window.html'));
-});
-
-app.get('/new-bucket', function (req, res) {
-  Bucket.create().then(bucket => {
-    res.redirect(302, '/app/' + bucket._id);
-  }, err => {
-    res.status(500).send('ERROR');
-  });
-});
-
-app.get('/app/:bucketId', function (req, res) {
-  res.send('BUCKET ID:' + req.params.bucketId);
-});
-
-app.use(compress()).options('*', cors()).use(cors()).use(favicon(path.join(app.get('public'), 'favicon.ico'))).use('/', serveStatic(app.get('public'))).use(bodyParser.json()).use(bodyParser.urlencoded({ extended: true })).configure(hooks()).configure(rest()).configure(socketio()).configure(services).configure(middleware);
 
 module.exports = app;
