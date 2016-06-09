47f4e325c55bc65a570617af54802a87193130dd
Nikica Jokic <neektza@gmail.com>
2016-06-09 13:13:49 +0200

    Prepare for Heroku.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node app/index.js
+web: MIX_ENV=prod mix phoenix.server
--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule Writisan.UserSocket do
   # channel "rooms:*", Writisan.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
+
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
